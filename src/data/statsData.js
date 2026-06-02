4396dde3c03046dcea6c1e9a5bb172d74b08e63e
taufik-shf <taufik.shf@gmail.com>
2026-06-02 14:56:01 +0700

    fix(ci): restore statsData (still used by Hero) and sync i18n

---
+++ src/data/statsData.js
@@ -0,0 +1,78 @@
+import { translate } from "@docusaurus/Translate";
+
+export const statsData = {
+  stats: [
+    {
+      id: "01",
+      value: translate({
+        id: "homepage.stats.01",
+        message: "50TB+",
+        description: "Statistics Number 01",
+      }),
+      label: translate({
+        id: "homepage.stats.01.label",
+        message: "Total Data Tersedia",
+        description: "Statistics Label 01",
+      }),
+      description: translate({
+        id: "homepage.stats.01.description",
+        message: "Data satelit dan geospasial yang tersedia untuk diakses",
+        description: "Statistics Description 01",
+      }),
+    },
+    {
+      id: "02",
+      value: translate({
+        id: "homepage.stats.02",
+        message: "100%",
+        description: "Statistics Number 02",
+      }),
+      label: translate({
+        id: "homepage.stats.02.label",
+        message: "Cakupan Wilayah Indonesia",
+        description: "Statistics Label 02",
+      }),
+      description: translate({
+        id: "homepage.stats.02.description",
+        message: "Seluruh wilayah Negara Kesatuan Republik Indonesia",
+        description: "Statistics Description 02",
+      }),
+    },
+    {
+      id: "03",
+      value: translate({
+        id: "homepage.stats.03",
+        message: "30+",
+        description: "Statistics Number 03",
+      }),
+      label: translate({
+        id: "homepage.stats.03.label",
+        message: "Tahun Data Historis",
+        description: "Statistics Label 03",
+      }),
+      description: translate({
+        id: "homepage.stats.03.description",
+        message: "Arsip data satelit dari berbagai konstelasi misi penginderaan jauh",
+        description: "Statistics Description 03",
+      }),
+    },
+    {
+      id: "04",
+      value: translate({
+        id: "homepage.stats.04",
+        message: "Gratis",
+        description: "Statistics Number 04",
+      }),
+      label: translate({
+        id: "homepage.stats.04.label",
+        message: "Akses Terbuka untuk publik",
+        description: "Statistics Label 04",
+      }),
+      description: translate({
+        id: "homepage.stats.04.description",
+        message: "Data dan layanan API terbuka tanpa biaya untuk masyarakat",
+        description: "Statistics Description 04",
+      }),
+    },
+  ],
+};
